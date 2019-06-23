ee09af1a676ff8c32b993b473b8d96ed3ecfd020
Darin Morris <darin@darinm.com>
2019-06-24 00:30:41 +0200

    Installed sphinx_rtd_theme.

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'howzitapp-docs'
-copyright = '2019, Darin Morris'
+copyright = '2019, Howzit Social App'
 author = 'Darin Morris'
 
 
@@ -33,6 +33,7 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -44,7 +45,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
